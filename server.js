673dd4b735c822ba7424252d89fad2564d6bb6f3
sahil kushwaha <sahilkushwaha5510@gmail.com>
2025-02-12 02:34:27 +0530

    add dockerfile and spin 3 instance of nodejs application with the help of docker compose

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import path from 'path'
 dotenv.config()
 const PORT = process.env.PORT || 5000
+const replica = process.env.APP_NAME
 
 const app = express()
 
@@ -10,9 +10,13 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended:true, limit:'1mb'}))
 app.use(express.json())
 
-
+app.use('/' , (req,res)=>{
+  
+    res.sendFile('index.html',{root:'public'})
+    console.log("response sent from "+ replica ) 
+})
 
 
 app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`${replica}Server is running on port ${PORT}`)
 })
